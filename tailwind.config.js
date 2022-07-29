7796bdf151ad189224c4dd5364246e0cfa855ffd
conqueror-G <gitconquerorg@gmail.com>
2022-07-29 10:18:45 +0900

    MODIFY: common font, custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,42 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundColor: {
-        default: '#FAFAFA',
-        Paper: '#FFFFFF',
-        Primary: '#036DB7',
+      colors: {
+        //Primary
+        primary: '#036DB7',
+        //Text
+        textPrimary: '#262E38',
+        textSecondary: '#696969',
+        textDisabled: '#9E9E9E',
+        //Acc
+        accError: '#DC6B6D',
+        accPoint: '#DC6B6D',
+        accCorrect: '#21BF64',
+        accDivider: '#C9C9C9',
+        accDividerTable: '#F2F8FB',
+        accOrange100: '#FD9009',
+        accOrange60: '#FEBC6B',
+        //Btn
+        btnActive: '#696969',
+        btnDisabled: '#AAAAAA',
+        btnDisabledBg: '#C9C9C9',
+        btnSelect: '#D3D3D3',
+        btnSelectBg: '#F6F6F6',
+        btnStroke: '#D3D3D3',
+        btnHover: '#E4E4E4',
+        btnActionSelect: '#E2E2E2',
+        //Bg
+        bgDefault: '#FAFAFA',
+        bgPaper: '#FFFFFF',
+        //Dots
+        dot1: '#47BEFF',
+        dot2: '#1CDFBB',
+        dot3: '#FFC506',
+        dot4: '#FF4C65',
+        dot5: '#E391FF',
+        dot6: '#2850A0',
+        dot7: '#FDE2Ad',
+        dot8: '#AD8080',
       },
     },
   },
